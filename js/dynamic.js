3d4fb9f1bce21783d1cec8f6ae07961faebe14a5
RAlhayek <>
2021-04-18 13:52:02 +0000

    new change

--- js/dynamic.js
+++ js/dynamic.js
@@ -1,13 +1,7 @@
-<<<<<<< HEAD
-alert ('Wlcome to your healthy life style guide:)');
-=======
-
 
 alert ('Wlcome to your healthy life style guide:)');
 
-var userAge = prompt('Enter your age')
-var age1 = 26;
->>>>>>> f9769f2c48b98bb1df7fdcc493d42c0e29409288
+
 
 /*var userAge = prompt('Enter your age')
 var age= 26;
@@ -41,10 +35,7 @@ while (foodtype !== 'protein' && foodtype !== 'fruit'){
   { var image2 = '<img src="https://image.sciencenorway.no/1438480.jpg?imageId=1438480&panow=0&panoh=0&panox=0&panoy=0&heightw=0&heighth=0&heightx=0&heighty=0&width=1200&height=630"alt="fruit"/>';
   document.write(image2);
   }
-<<<<<<< HEAD
-=======
 
->>>>>>> f9769f2c48b98bb1df7fdcc493d42c0e29409288
 
 var image3 = '<img src="https://d50b62f6164e0c4a0279-11570554cb5edae3285603e6ab25c978.ssl.cf5.rackcdn.com/html_body_blocks/images/000/013/890/original/EasyHealthySnacks_en65ab5213130c9862172ac11435f055d9_en38b28edc7b2830a46f6e00bfeceeb1b6.jpg?1596090039"alt="snack"/>';
 var snackNum = prompt ( 'How many snacks do you have per day?')
@@ -52,10 +43,7 @@ for (var i = 1; i <= snackNum; i++){
  document.write(image3)
 
 }
-<<<<<<< HEAD
-=======
 
->>>>>>> f9769f2c48b98bb1df7fdcc493d42c0e29409288
 
 var image3 = '<img src="https://d50b62f6164e0c4a0279-11570554cb5edae3285603e6ab25c978.ssl.cf5.rackcdn.com/html_body_blocks/images/000/013/890/original/EasyHealthySnacks_en65ab5213130c9862172ac11435f055d9_en38b28edc7b2830a46f6e00bfeceeb1b6.jpg?1596090039"alt="snack"/>';
 var snackNum = prompt ( 'How many snacks do you have per day?')
@@ -82,5 +70,4 @@ function askUser(){
   
 }
 }
-
-
+askUser();
